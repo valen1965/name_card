d179a4183643263cf66d34a4a3af9ce8b7d22564
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2024-05-14 03:15:33 +0300

    third commit

--- server.py
+++ server.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def home_page():
     return render_template("index.html")
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+#
+# if __name__ == "__main__":
+#     app.run(debug=True)
